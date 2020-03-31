0cd2637d86caaa9fbba30925b720527134130675
yangkai <1548580932@qq.com>
2020-03-31 15:55:56 +0000

    zuojia

--- routes/web.php
+++ routes/web.php
@@ -49,9 +49,10 @@ Route::get('/lists/{id}','Index\IndexController@lists');
 
 Route::get('/author_do','Index\IndexController@author_do');
 
-Route::get('/author_do2','Index\IndexController@author_do2');
+Route::post('/author_do2','Index\IndexController@author_do2');
 
 Route::get('/author_reg','Index\IndexController@author_reg');
+
 
 
 
