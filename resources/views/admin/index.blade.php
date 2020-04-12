c4df50a25d0586a8381b971a06c5f62af0ee1760
yangkai <1548580932@qq.com>
2020-04-12 11:00:36 +0000

    author

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="{{asset('css/font.css')}}">
     <link rel="stylesheet" href="{{asset('css/xadmin.css')}}">
     <!-- <link rel="stylesheet" href="./css/theme5.css"> -->
-    <script src="{{asset('lib/layui/layui.js')}}}" charset="utf-8"></script>
+    <script src="{{asset('lib/layui/layui.js')}}" charset="utf-8"></script>
     <script type="text/javascript" src="{{asset('js/xadmin.js')}}"></script>
     <!-- 让IE8/9支持媒体查询，从而兼容栅格 -->
     <!--[if lt IE 9]>
